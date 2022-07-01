a77fc852c4586a5e3e7bc6e25862b3d569abb246
nguyenanhung <info@nguyenanhung.com>
2022-07-01 16:16:45 +0700

    IP

---
+++ plugins/opcache.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Project make-some-noise
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 01/07/2022
+ * Time: 16:15
+ */
+require_once __DIR__ . '/../vendor/amnuts/opcache-gui/index.php';
